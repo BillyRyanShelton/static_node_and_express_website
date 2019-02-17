133aac31011b06e0ca80f06fc777309925ecdf68
Billy Shelton <sheltonbilly@utexas.edu>
2019-02-17 13:45:26 -0600

    fixed the project.pug file to correctly display project images.

--- routes/projects.js
+++ routes/projects.js
@@ -7,9 +7,21 @@ router.get('/projects', (req, res) => {
 });
 
 router.get('/projects/:id', (req, res, next) => {
-	const id = req.params.id;
-	if(id > 0 && id <= projects.length) {
-		res.render('project');	
+	let id = req.params.id;
+
+	if(id >= 0 && id <= projects.length-1) {
+		let projectData = projects[id];
+		
+		res.render('project', {
+			projectName : projects[id].project_name,
+			projectDesc : projects[id].description,
+			projectTech : projects[id].technologies,
+			projectLink : projects[id].live_link,
+			projectGit : projects[id].github_link,
+			projectImg1 : projects[id].image_urls[0],
+			projectImg2 : projects[id].image_urls[1],
+			projectImg3 : projects[id].image_urls[2],
+		});	
 	} else {
 		next();
 	}
